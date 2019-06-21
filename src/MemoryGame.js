e00d8a65854bde897fa3c5b4a000468fdf7101f1
Daniel Tovar <daniel@danieltovar.io>
2019-06-20 22:35:20 -0600

    added click handler function to Cards

--- src/MemoryGame.js
+++ src/MemoryGame.js
@@ -22,100 +22,127 @@ export default class MemoryGame extends Component {
       {
         id: 0,
         cardState: CardState.HIDDEN,
-        backgroundImage: 'linear-gradient(90deg, #FC466B 0%, #3F5EFB 100%)'
+        backgroundImage: 'linear-gradient(180deg, #FC466B 0%, #3F5EFB 100%)'
       },
       {
         id: 1,
         cardState: CardState.HIDDEN,
-        backgroundImage: 'linear-gradient(90deg, #FC466B 0%, #3F5EFB 100%)'
+        backgroundImage: 'linear-gradient(180deg, #FC466B 0%, #3F5EFB 100%)'
       },
       {
         id: 2,
         cardState: CardState.HIDDEN,
-        backgroundImage: 'linear-gradient(90deg, #d53369 0%, #daae51 100%)'
+        backgroundImage: 'linear-gradient(180deg, #d53369 0%, #daae51 100%)'
       },
       {
         id: 3,
         cardState: CardState.HIDDEN,
-        backgroundImage: 'linear-gradient(90deg, #d53369 0%, #daae51 100%)'
+        backgroundImage: 'linear-gradient(180deg, #d53369 0%, #daae51 100%)'
       },
       {
         id: 4,
         cardState: CardState.HIDDEN,
-        backgroundImage: 'linear-gradient(90deg, #0700b8 0%, #00ff88 100%)'
+        backgroundImage: 'linear-gradient(180deg, #0700b8 0%, #00ff88 100%)'
       },
       {
         id: 5,
         cardState: CardState.HIDDEN,
-        backgroundImage: 'linear-gradient(90deg, #0700b8 0%, #00ff88 100%)'
+        backgroundImage: 'linear-gradient(180deg, #0700b8 0%, #00ff88 100%)'
       },
       {
         id: 6,
         cardState: CardState.HIDDEN,
-        backgroundImage: 'linear-gradient(90deg, #00C9FF 0%, #92FE9D 100%)'
+        backgroundImage: 'linear-gradient(180deg, #00C9FF 0%, #92FE9D 100%)'
       },
       {
         id: 7,
         cardState: CardState.HIDDEN,
-        backgroundImage: 'linear-gradient(90deg, #00C9FF 0%, #92FE9D 100%)'
+        backgroundImage: 'linear-gradient(180deg, #00C9FF 0%, #92FE9D 100%)'
       },
       {
         id: 8,
         cardState: CardState.HIDDEN,
-        backgroundImage: 'linear-gradient(90deg, #9ebd13 0%, #008552 100%)'
+        backgroundImage: 'linear-gradient(180deg, #9ebd13 0%, #008552 100%)'
       },
       {
         id: 9,
         cardState: CardState.HIDDEN,
-        backgroundImage: 'linear-gradient(90deg, #9ebd13 0%, #008552 100%)'
+        backgroundImage: 'linear-gradient(180deg, #9ebd13 0%, #008552 100%)'
       },
       {
         id: 10,
         cardState: CardState.HIDDEN,
-        backgroundImage: 'linear-gradient(90deg, #3F2B96 0%, #A8C0FF 100%)'
+        backgroundImage: 'linear-gradient(180deg, #3F2B96 0%, #A8C0FF 100%)'
       },
       {
         id: 11,
         cardState: CardState.HIDDEN,
-        backgroundImage: 'linear-gradient(90deg, #3F2B96 0%, #A8C0FF 100%)'
+        backgroundImage: 'linear-gradient(180deg, #3F2B96 0%, #A8C0FF 100%)'
       },
       {
         id: 12,
         cardState: CardState.HIDDEN,
-        backgroundImage: 'linear-gradient(90deg, #fcff9e 0%, #c67700 100%)'
+        backgroundImage: 'linear-gradient(180deg, #fcff9e 0%, #c67700 100%)'
       },
       {
         id: 13,
         cardState: CardState.HIDDEN,
-        backgroundImage: 'linear-gradient(90deg, #fcff9e 0%, #c67700 100%)'
+        backgroundImage: 'linear-gradient(180deg, #fcff9e 0%, #c67700 100%)'
       },
       {
         id: 14,
         cardState: CardState.HIDDEN,
-        backgroundImage: 'linear-gradient(90deg, #1CB5E0 0%, #000851 100%)'
+        backgroundImage: 'linear-gradient(180deg, #1CB5E0 0%, #000851 100%)'
       },
       {
         id: 15,
         cardState: CardState.HIDDEN,
-        backgroundImage: 'linear-gradient(90deg, #1CB5E0 0%, #000851 100%)'
+        backgroundImage: 'linear-gradient(180deg, #1CB5E0 0%, #000851 100%)'
       }
     ];
     cards = shuffle(cards);
     this.state = { cards, noClick: false };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  // ! Takes ID of card that was clicked, so we know which one to modify in the state array
+  handleClick(id) {
+    this.setState(prevState => {
+      // ! Map through each card
+      let cards = prevState.cards.map(card =>
+        // ! If ID's match... Change the set from hidden to matched, or matched to hidden
+        card.id === id
+          ? {
+              ...card,
+
+              cardState:
+                card.cardState === CardState.HIDDEN
+                  ? CardState.MATCHED
+                  : CardState.HIDDEN
+            }
+          : // ! If ID's don't match... return card as-is
+            card
+      );
+      return { cards };
+    });
   }
 
   render() {
-    // Iterate over cards in state and return a Card component
+    // * Iterate over cards in state and return a Card component
     const cards = this.state.cards.map(card => (
       <Card
         key={card.id}
-        visible={true}
+        // ! Only show card if not equal to HIDDEN
+        visible={card.cardState !== CardState.HIDDEN}
         backgroundImage={card.backgroundImage}
-        onClick={() => {}}
+        onClick={() => {
+          this.handleClick(card.id);
+        }}
       />
     ));
 
-    // return a Navbar + Cards
+    // * return a Navbar + Cards
     return (
       <div>
         <Navbar />
