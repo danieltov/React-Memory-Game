88e39c0802f01143e306e882a2a389231542b36a
Daniel Tovar <daniel@danieltovar.io>
2019-06-22 09:10:29 -0600

    Production build

---
+++ build/precache-manifest.33f332df9c004048abc353c9f321ebf8.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "abf1701ec7dcd59ac2a5846bdafd0d12",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4ba993a2ae3b12c9718",
+    "url": "/static/css/main.b2880a5f.chunk.css"
+  },
+  {
+    "revision": "d0581df5327a23d736c5",
+    "url": "/static/js/2.3c931148.chunk.js"
+  },
+  {
+    "revision": "a4ba993a2ae3b12c9718",
+    "url": "/static/js/main.e2b79dd2.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
